02f2ce06a71f46f1724a168406291a41ce9bd667
Lskram <dankflamemasterLove@gmail.com>
2024-09-04 21:18:50 +0700

    DAY one

---
+++ lib/login_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<Form>()
+}
